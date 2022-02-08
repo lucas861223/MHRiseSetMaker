16e3fb5dcc13155a204b92c5cfe228bf80d0163e
lucas861223 <lucas861223@gmail.com>
2022-02-07 16:00:33 -0800

    Populate SkillList

--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -1,8 +1,24 @@
-export interface Skills {
+export class Skills {
     name: string;
     maxLevel: number;
     decoLevel: number;
     description?: string;
     levelDescriptions?: string[];
-    desiredLevel: string;
+    desiredLevel = "0";
+
+    constructor(name: string, maxLevel: number, decoLevel: number, desiredLevel?: string, description?: string, levelDescriptions?: string[]) {
+        if (typeof description !== 'undefined') {
+            this.description = description;
+        }
+        if (typeof levelDescriptions !== 'undefined') {
+            this.levelDescriptions = levelDescriptions;
+        }
+        if (typeof desiredLevel !== 'undefined') {
+            this.desiredLevel = desiredLevel;
+        }
+        this.name = name;
+        this.maxLevel = maxLevel;
+        this.decoLevel = decoLevel;
+
+    }
 }
